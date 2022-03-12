7ad5e6536741997862d4feb545bc9738b91f4e30
Kondwani David. Mwale <kondwanim@livestock.local>
2022-03-12 19:36:59 +0200

    users api folder

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 require('dotenv').config();
 const taskRoute = require('./routes/tasks')
 const cattleRoute = require('./routes/cattle')
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/tasks', taskRoute);
 app.use('/cattle', cattleRoute);
+app.use('/auth', authRoute);
 
 // Default Route
 app.get('/', (req,res) =>{
