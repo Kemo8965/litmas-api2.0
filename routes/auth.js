f8bed3d7269ad113d66044f43d64ca8c0c1c2cef
Kondwani David. Mwale <kondwanim@livestock.local>
2022-05-02 21:18:35 +0200

    cors error new headers

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,6 @@ const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors')
-const app = express();
-
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
 
 router.get('/', async (req,res)=>{
   
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,14 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
 const User = require('../models/User');
-const app = express();
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
   
 router.get('/', async (req,res)=>{
   
