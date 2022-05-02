346c03e7040b116c764da8e14099ccdfe211d49b
Kondwani David. Mwale <kondwanim@livestock.local>
2022-05-02 19:26:30 +0200

    cors error still coming up. commented out headers on corsOptions

--- app.js
+++ app.js
@@ -31,7 +31,14 @@ const bodyParser = require("body-parser")
 //   next();
 // });
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+//app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/tasks', taskRoute);
