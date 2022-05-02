38d99997ffd56cbc8cfd2b7b934283b0dabea5b0
Kondwani David. Mwale <kondwanim@livestock.local>
2022-05-02 20:16:43 +0200

    cors error still coming up. commented out headers on corsOptions

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors')
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 router.get('/', async (req,res)=>{
   
        res.send('Authentication Route is grafting');
@@ -54,7 +61,7 @@ router.get('/User/:id', async (req,res)=>{
 
  
   //LOGIN  USERS
-    router.post('/login', async (req,res)=>{
+    router.post('/login',cors(), async (req,res)=>{
 
 
      
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const Task = require('../models/Task');
 const User = require('../models/User');
 
-
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 router.get('/', async (req,res)=>{
   
     res.send('Tasks Route is grafting!');
