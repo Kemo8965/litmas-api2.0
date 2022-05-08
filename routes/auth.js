f29c9844ac8b72341db3a29cc3e2fa1adea7a09d
Kondwani David. Mwale <kondwanim@livestock.local>
2022-05-09 00:43:58 +0200

    added HTTP ok status to /login

--- routes/auth.js
+++ routes/auth.js
@@ -165,7 +165,7 @@ router.put('/activateUser/:id', async (req,res,next )=>{
 
     router.get('/logout', async (req,res) => {
         res.header('auth-token', '', { maxAge: 1 });
-        res.status(200).send("OK");
+       // res.status(200).send("OK");
         res.redirect('/auth/login').send('Logged Out!');
     });
 
